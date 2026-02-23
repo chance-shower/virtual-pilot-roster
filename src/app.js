97d44ec2ea1e81957f14c37b908e96e0abc59ae6
chance-shower <104402848+chance-shower@users.noreply.github.com>
2026-02-23 16:30:57 +1300

    more coding updates

--- src/app.js
+++ src/app.js
@@ -24,19 +24,46 @@ loadFlightData();
 // Function to generate roster
 
 async function createTrip() {
+
+    if (!flightData) {
+        alert("Flight schedules are still loading - please try again in a few seconds...");
+    }
     // Get user input
-    const airline = document.getElementById('airlineCode').value;
-    const equipment = document.getElementById('equipmentCode').value;
-    const homeBase = document.getElementById('homeBase').value;
+    const airline = document.getElementById('airlineCode').value.toUpperCase().trim();
+    // Handle multiple aircraft
+    const equipment = document.getElementById('equipmentCode').value
+        .split(',')
+        .map(s => s.trim().toUpperCase());
+    const homeBase = document.getElementById('homeBase').value.toUpperCase().trim();
     const dutyLength = document.getElementById('dutyLength').value;
-    const desiredAirports = document.getElementById('desiredAirports')?.value;
-    const excludedAirports = document.getElementById('excludedAirports')?.value;
+    const desiredAirports = document.getElementById('desiredAirports')?.value
+        .split(',')
+        .map(s => s.trim().toUpperCase());
+    const excludedAirports = document.getElementById('excludedAirports')?.value
+        .split(',')
+        .map(s => s.trim().toUpperCase());
 
     // Show spinner while timetable is generated
     document.getElementById('loader-overlay').style.display = 'flex';
 
-    //try {
-    //    const baseData = flightData[homeBase]?.[airline];
-    //}
+    // Build first route
+
+    try {
+        // Filter by homebase and airline to start
+        const firstLegData = flightData[homeBase]?.[airline];
+        if (!firstLegData) throw `No flights found for ${airline} at ${homeBase}...`;
+
+        // Filter all flights from above for the chosen aircraft type
+        let pool = [];
+        equipment.forEach(aircraft => {
+            if (firstLegData[aircraft]) {
+                Object.keys(firstLegData[aircraft]).forEach(dest => {
+                    firstLegData[aircraft][dest].forEach(flt => {
+                        pool.push({ ...flt, dep: homeBase, arr: dest, equip: aircraft});
+                    });
+                });
+            }      
+        });
+    }
 
 }
